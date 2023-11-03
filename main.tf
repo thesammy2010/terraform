284f4151c1c4bf755d5c9e76f0c11896635f1815
thesammy2010 <43960199+thesammy2010@users.noreply.github.com>
2023-11-03 00:44:57 +0000

    Cloudflare: thesammy2010/DNS zones: import all TXT values

--- main.tf
+++ main.tf
@@ -5,16 +5,28 @@ module "cloudflare" {
   cloudflare_zone_id    = var.cloudflare_zone_id
   txt_records = [
     {
-      "name" : "test",
-      "value" : "foobar"
+      "name" : "_dmarc.mail",
+      "value" : "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s",
       "proxied" : false,
-      "comment" : "This is a test via Terraform"
+      "comment" : null
     },
     {
       "name" : "mail"
       "value" : "v=spf1 include:simplelogin.co ~all",
       "proxied" : false,
       "comment" : null
-    }
+    },
+    {
+      "name" : "mail",
+      "value" : "sl-verification=iexaqmuqgizrxuitllmharfijvpqla",
+      "proxied" : false,
+      "comment" : null
+    },
+    {
+      "name" : "test",
+      "value" : "foo=bar"
+      "proxied" : false,
+      "comment" : "This is a test via Terraform"
+    },
   ]
 }
